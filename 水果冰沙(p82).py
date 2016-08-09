efd208430aa80aa75196b5135abb8303529a7cfd
mmxmmxmmx <mmxmmxmmx1@gmail.com>
2016-08-09 15:31:33 +0800

    Add files via upload

---
+++ 水果冰沙(p82).py
@@ -0,0 +1,15 @@
+import sys
+def make_smoothie():
+    juice = input("What juice would you like? ")
+    fruit = input("OK - and how about the fruit? ")
+    print("Thanks.Let's go!")
+    print("Crushing the ice .. ")
+    print("Blending the ice... ")
+    print("Now adding in the " + juice + " juice ")
+    print("Finished! There's your " + fruit + " and " + juice + " smoothie! ")
+
+print("Welcome to smoothie-matic 2.0")
+another = "Y"
+while another == "Y":
+  make_smoothie()   
+  another = input("How about another(Y/N)?  ")
