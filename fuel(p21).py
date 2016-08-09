45906b0338c370e6c6b8f67d21a014d502b85357
mmxmmxmmx <mmxmmxmmx1@gmail.com>
2016-08-09 15:27:15 +0800

    Add files via upload

---
+++ fuel(p21).py
@@ -0,0 +1,9 @@
+g =input("checking your car remain fuel: ")
+guess =int(g)
+if guess >= 3:
+    print("It's OK!")
+    print("You can driver to downtown")
+else:
+    print("Sorry! You don't have enough fuel")
+    print("what's  next?")
+    
